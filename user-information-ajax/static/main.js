e851d1d798fd0a50775f4e8fd2674192256596e8
Jimmy <jimmyvoskuil@msn.com>
2016-10-27 14:29:28 +0200

    added new indexof() function

--- user-information-ajax/static/main.js
+++ user-information-ajax/static/main.js
@@ -1,13 +1,14 @@
 alert("Hello World")
 
 function myFunction() {
-			let userinput = $("#searchbar").val();
+			let userinput = $("#searchbarid").val();
 			$.post('/searchajax',userinput,(data, status) =>{
 				console.log(data)
+				console.log(some)
 			})
 		}
 		$('#searchbarid').keyup(function(){
-			console.log('Key was pressed')
+			// console.log('Key was pressed') This Works fine :) 
 				myFunction()
 		})
 

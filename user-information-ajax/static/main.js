fb3cba469edad46d8112c6c3dbeafdc4f30482f7
Jimmy <jimmyvoskuil@msn.com>
2016-11-01 16:47:59 +0100

    fixed my SetTimeout Function and now it works

--- user-information-ajax/static/main.js
+++ user-information-ajax/static/main.js
@@ -1,44 +1,46 @@
 // This alert was to test to see if the JavaScript was loaded :)
 // alert("Hello World")
 
-$(document).ready(function() {}
+$(document).ready(function() {
 
 	function myFunction() {
-				let userinput = $("#searchbarid").val();
-				$.post('/searchajax',{data: userinput},(data, status) =>{
-					//  This is working, the data is being console.log
-					// console.log(data)
-					console.log(typeof data)
-					// document.write(data);
-				
-					$('.autocomplete').empty()
-					$( ".autocomplete" ).append("<p>" + data + "</p>")
+		let userinput = $("#searchbarid").val();
+		$.post('/searchajax',{data: userinput},(data, status) =>{
+			//  This is working, the data is being console.log
+			// console.log(data)
+			console.log(typeof data)
+			console.log ("I work")
+			// document.write(data);
 
-				})
-			}
+			$('.autocomplete').empty()
+			$( ".autocomplete" ).append("<p>" + data + "</p>")
+
+		})
+	}
 
 
-
+	let AjaxCanRun = true
 
 	$('#searchbarid').keyup(function(){
-
 		if (AjaxCanRun) {
 
-			AjaxCanRun = false
-
-		/// If false, the fuction will not run, if true it will runn
-		//
 		// console.log('Key was pressed') This Works fine :) 
 		// if (300 second have passed) {
 			myFunction()
 			// start counter
-		}
-	}
-	// This set time out will make sure to see if the fuction can run
 
-	setTimeout(function() {
+		/// If false, the fuction will not run, if true it will runn
+			AjaxCanRun = false
+		}
+
+		// This set time out will make sure to see if the fuction can run
+
+		setTimeout(function() {
 		AjaxCanRun = true
-	}, 300)
+			}, 	3000)
+
+	})
+})
 
 
 
